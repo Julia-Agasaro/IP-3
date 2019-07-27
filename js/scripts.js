66bbbaa525f6f934a8e76de9f679c58ab45de46f
Julia-Agasaro <agasarojulia@gmail.com>
2019-07-27 20:45:52 +0200

    Added paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
      $('.design').hide();
      $('.image').show();
  });
+ $('.image2').click(function(){
+    $('.development').show();
+    $('.image2').hide();
+    });
+    $('.development').click(function(){
+        $('.development').hide();
+        $('.image2').show();
+    });
+    $('.image3').click(function(){
+        $('.product').show();
+        $('.image3').hide();
+        });
+        $('.product').click(function(){
+            $('.product').hide();
+            $('.image3').show();
+        });
+    
+
 });
